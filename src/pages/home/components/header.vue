695ef2d7a42b183ae4aa9c11c01bb0110c5e32b1
410178050@qq.com <410178050@qq.com>
2019-01-08 14:43:46 +0800

    7-2

--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="header">
-      <div class="header-left">返回</div>
-       <div class="header-input">输入城市/景点/游玩主题/</div>
-       <div class="header-right">城市</div>
+       <div class="header-left">
+         <div class="iconfont back-icon">&#xe625;</div>
+       </div>
+       <div class="header-input">
+         <span class="iconfont">&#xe639;</span>
+         输入城市/景点/游玩主题
+       </div>
+       <div class="header-right">城市
+          <span class="iconfont arrow-icon">&#xe6aa;</span>
+       </div>
     </div>
 </template>
 <script>
@@ -18,7 +25,11 @@ export default {
     color: #ffffff
     .header-left
       width: .68rem
+      height: .86rem
       float: right
+      .back-icon
+        font-size: .4rem
+        text-align: center
     .header-input
       flex: 1
       background: #ffffff
@@ -29,8 +40,14 @@ export default {
       margin-left: .2rem
       margin-right: .2rem
       color: #cccccc
+      padding-left: .2px
+      span
+        padding-left .2rem
     .header-right
       width: 1.24rem
       float: right
-      text-align: center
+      .arrow-icon
+        font-size: .3rem
+        margin-left: -.05rem
+
 </style>
