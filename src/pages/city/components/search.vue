28f3c26da9b8333faa8b3b1a2800138a65732f49
410178050@qq.com <410178050@qq.com>
2019-01-16 17:52:46 +0800

    8-2

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+      <input class="search-input" type="text" placeholder="输入城市名称或拼音">
+  </div>
+</template>
+<script>
+  export default {
+    name: 'CitySearch'
+  }
+</script>
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/iconfont/varibles.styl'
+  .search
+    background:#00bcd4
+    height: .72rem
+    padding: 0  .1rem
+    .search-input
+      background:#ffffff
+      border-radius:5px
+      text-align: center
+      width: 100%
+      height: .62rem
+      padding 0 .1rem
+      box-sizing: border-box
+      line-height: .62rem
+</style>
