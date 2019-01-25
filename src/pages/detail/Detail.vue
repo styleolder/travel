ae104a53db54504f532de052314df5b712f86861
410178050@qq.com <410178050@qq.com>
2019-01-25 14:54:13 +0800

    9-5

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,13 +2,14 @@
   <div class="detail">
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <detail-content></detail-content>
+    <detail-content :list="categoryList"></detail-content>
   </div>
 </template>
 <script>
 import DetailBanner from './compinents/banner'
 import DetailHeader from './compinents/header'
 import DetailContent from './compinents/content'
+import axios from 'axios'
 
 export default {
   name: 'Detail',
@@ -16,6 +17,30 @@ export default {
     DetailContent,
     DetailHeader,
     DetailBanner
+  },
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      categoryList: []
+    }
+  },
+  methods: {
+    getDetailList () {
+      axios.get('/api/detail.json')
+        .then(this.getDetailListSucc)
+    },
+    getDetailListSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.categoryList = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailList()
   }
 }
 </script>
--- src/pages/detail/compinents/content.vue
+++ src/pages/detail/compinents/content.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="container">
+    <div v-for="(item, index) in list" :key="index" class="detail-info">
+      <div class="detail-title border-bottom">
+        <span class="iconfont detail-icon">&#xe65b;</span>
+          {{ item.title }}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <detail-content :list="item.children"></detail-content>
+      </div>
+    </div>
   </div>
 </template>
 <script>
   export default {
-    name: 'DetailContent'
+    name: 'DetailContent',
+    props: {
+      list: Array
+    }
   }
 </script>
 <style lang="stylus" scoped>
-  .container
-    display: inline-block
-    width: 100%
-    height: 1000px
-    background: #eaeaea
+  .detail-title
+    line-height: .8rem
+    font-size: .26rem
+    padding: 0 .2rem
+  .item-children
+    padding: 0 .36rem
 </style>
