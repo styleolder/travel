3542f6e07ddd75547b8a43f377e05d56fa1d9c53
410178050@qq.com <410178050@qq.com>
2019-01-25 20:51:55 +0800

    10-4

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://192.168.0.102:8080',
         pathRewrite: {
           '^/api': '/static/mock'
         }
@@ -58,7 +58,6 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-
     /**
      * Source Maps
      */
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
+      // component: () => import('@/pages/home/Home')
       component: Home
     },
     {
